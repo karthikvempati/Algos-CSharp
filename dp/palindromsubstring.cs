ebed1c6f897f089e648749a6332ef274f0ccc994
Karthik Vempati <karthikvempati@gmail.com>
2019-01-06 13:05:46 -0600

    Update palindromsubstring.cs

--- dp/palindromsubstring.cs
+++ dp/palindromsubstring.cs
@@ -10,7 +10,7 @@ public static class Extensions
         {
             end = source.Length + end;
         }
-        int len = end - start;               // Calculate length
+        int len = end - start+1;               // Calculate length
         return source.Substring(start, len); // Return Substring of length
     }
 }
@@ -19,13 +19,17 @@ public class Solution {
     public string LongestPalindrome(string s) {
         if(s.Length == 1)
             return s;
+        if(s.Length == 2 && s[0] != s[1]){
+            return s.Substring(0,1);
+        }
         var result = String.Empty;
         Dictionary<string, bool> memo = new Dictionary<string, bool>();
         for(int i=0;i<s.Length-1;i++){
             for(int j=i; j< s.Length;j++){
-                if(len <= result.Length) continue; 
-                if(len > result.Length && IsPalindrome(i,j, s, memo)){
-                    result = s.Slice(i,j);
+                if(IsPalindrome(i,j, s, memo)){
+                    string c = s.Slice(i,j);
+                    if(result.Length < c.Length)
+                        result = c;
                 }
             }
         }
@@ -33,25 +37,30 @@ public class Solution {
         return result;
     }
     
-    public bool IsPalindrome(int start, int end,string s, Dictionary<string, bool> memo){
-        if(i >= j){
-            if(memo.ContainsKey(i+"$"+j))
-                return memo[i+"$"+j];
+    public bool IsPalindrome(int i, int j,string s, Dictionary<string, bool> memo){
+        if(i < j ){
+            var lKey = i+"$"+j;
+            if(memo.ContainsKey(lKey))
+                return memo[lKey]; 
             bool doEndsMatch = s[i] == s[j];
-            i = i+1;
-            j=j-1;
-            var key = i+"$"+j;
+            var a= i+1;
+            var b= j-1;
+            var key = a+"$"+b;
             bool subPalindrome = memo.ContainsKey(key) ? 
-                memo[key] : IsPalindrome(i,j,s, memo);
+                memo[key] : IsPalindrome(a,b,s, memo);
             bool result = doEndsMatch && subPalindrome;
-            memo.Add(key,result);
+            memo.Add(lKey,result);
             return result;
         }
-        else
+        else if(i > 0 && j > 0 )
         {
             var key = i+"$"+j;
+            if(memo.ContainsKey(key))
+                return memo[key]; 
             memo.Add(key,true);
             return true;
         }
+        
+        return false;
     }
 }
