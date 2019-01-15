227af94e00a57135ee27879dfb09654bd93a0dfc
Karthik Vempati <karthikvempati@gmail.com>
2019-01-14 21:01:18 -0600

    Update WordSearch.cs

--- MockInterviews/WordSearch.cs
+++ MockInterviews/WordSearch.cs
@@ -14,33 +14,38 @@ namespace MockInterviews
          public bool Dfs(char[,] board, string word){
              bool[,] v = new bool[board.GetLength(0), board.GetLength(1)];
              
-             if(DfsUtil(board, word, 0,0,0,v)) return true;
+             for(int i=0;i<board.GetLength(0);i++){
+                 for(int j=0;j < board.GetLength(1);j++){
+                     if(DfsUtil(board, word, i,j,0,v)) return true;
+                 }
+             }
+             
+             /*if(DfsUtil(board, word, 0,0,0,v)) return true;
              if(board.GetLength(0) > 1 && DfsUtil(board, word, 1,0,0,v)) return true;
              if(board.GetLength(1) > 1 && DfsUtil(board, word, 0,1,0,v)) return true;
-             if(board.GetLength(1) > 1 && board.GetLength(0) > 1 && DfsUtil(board, word, 1,1,0,v)) return true;
+             if(board.GetLength(1) > 1 && board.GetLength(0) > 1 && DfsUtil(board, word, 1,1,0,v)) return true;*/
 
              return false;
          }
 
          public bool DfsUtil(char[,] board, string word,int i,int j,int index,bool[,] v){
 
-             if(word[index] == board[i,j]){
-                 if(index == word.Length - 1) return true;
-                 index++;
-             } 
+            if (word[index] == board[i, j])
+            {
+                if (index == word.Length - 1) return true;
 
-             if(v[i,j]){
-                 return false;
-             }
+                index++;
+                  
+                v[i, j] = true;
 
-             v[i,j] = true;
-
-             if(i < board.GetLength(0)-1 && !v[i+1,j] && DfsUtil(board, word, i+1,j,index,v)) return true;
-             if(j < board.GetLength(1)-1 && !v[i,j+1] && DfsUtil(board, word, i,j+1,index,v)) return true;
-             if(i > 0 && !v[i-1,j] && DfsUtil(board, word, i-1,j,index,v)) return true;
-             if(j > 0 && !v[i,j-1] && DfsUtil(board, word, i,j-1,index,v)) return true;             
+                if (i < board.GetLength(0) - 1 && !v[i + 1, j] && DfsUtil(board, word, i + 1, j, index, v)) return true;
+                if (j < board.GetLength(1) - 1 && !v[i, j + 1] && DfsUtil(board, word, i, j + 1, index, v)) return true;
+                if (i > 0 && !v[i - 1, j] && DfsUtil(board, word, i - 1, j, index, v)) return true;
+                if (j > 0 && !v[i, j - 1] && DfsUtil(board, word, i, j - 1, index, v)) return true;
+            }
 
              return false;
          }
+
     }
 }
